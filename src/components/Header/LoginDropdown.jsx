63cf00990fd9eb87e05826833a1dba83dd05cdff
mafidul islam <mafidul22@gmail.com>
2026-08-04 15:25:10 +0530

    all-login page done

--- src/components/Header/LoginDropdown.jsx
+++ src/components/Header/LoginDropdown.jsx
@@ -28,8 +28,8 @@ const LoginDropdown = () => {
     return () => document.removeEventListener('keydown', handleEsc);
   }, []);
 
-  const AdminUrl = import.meta.env.VITE_APP_ADMIN_URL || 'https://ntccsl.nofinofinance.net/admin/login';
-  const UserUrl = import.meta.env.VITE_APP_USER_URL || 'https://ntccsl.nofinofinance.net/login';
+  const AdminUrl = import.meta.env.VITE_APP_ADMIN_URL || 'https://nofino.ccsltd.in/Admin_Login.aspx';
+  const UserUrl = import.meta.env.VITE_APP_USER_URL || 'https://nofino.ccsltd.in/User_Login.aspx';
 
   const loginOptions = [
     {
